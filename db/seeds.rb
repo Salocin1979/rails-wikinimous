753a7f6efe7355820fcade3caa0fc56ac539325e
Nicolas Bagnath <nicolas.bagnath@gmail.com>
2021-09-25 17:20:12 +0400

    Rails Wikinimous

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+10.times do
+    Article.create(
+      title: Faker::Book.author,
+      content: Faker::Fantasy::Tolkien.poem)
+end
